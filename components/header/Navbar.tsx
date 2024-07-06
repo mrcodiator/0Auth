c4c5873e424a94b485e7242272b59648c613dcf0
mrcodiator <mr.codiator@gmail.com>
2024-07-06 23:12:36 +0530

    git repo added

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { auth } from '@/auth'
 import AuthMenu from './AuthMenu';
+import { Github } from 'lucide-react';
 
 const Navbar = async () => {
     const session = await auth()
@@ -17,6 +18,11 @@ const Navbar = async () => {
                 </div>
                 <div className='flex justify-end items-center space-x-3'>
                     <ModeToggle />
+                    <Link href={"https://github.com/mrcodiator/0Auth"}>
+                        <Button variant={"outline"} size={"icon"}>
+                            <Github className='h-4 w-4' />
+                        </Button>
+                    </Link>
                     <AuthMenu session={session} />
                 </div>
             </div>
